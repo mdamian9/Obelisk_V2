fa19468b760c3f2a208f0d9a3ec80c9788da08ee
Miguel Damian <38478936+mdamian9@users.noreply.github.com>
2022-12-26 22:06:12 -0800

    Finished /signup route. User is created successfully. Added code to route to check if username or email already exists within db. Working on hashing user password during user creation.

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -24,4 +24,28 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// Hash user password before saving to database
+userSchema.pre('save', function(callback) {
+    let user = this;
+    // Break out if password hasn't changed
+    if (!user.isModified('password')) return callback();
+    // Hash new password (or if it has changed)
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return callback(err);
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return callback(err);
+            user.password = hash;
+            callback();
+        });
+    });
+});
+
+// verifyPassword method for user
+userSchema.methods.verifyPassword = (password, callback) => {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
 module.exports = mongoose.model('User', userSchema);
--- api/routes/index.router.js
+++ api/routes/index.router.js
@@ -9,28 +9,34 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/signup', (req, res, next) => {
-    res.status(201).json({ message: 'You have successfully created a new account!' });
-    console.log(req.body);
-
+    // Check if username or email already exists within db
     const promises = [
         db.User.findOne({ username: req.body.username }),
         db.User.findOne({ email: req.body.email })
     ];
-
     Promise.all(promises).then(values => {
-        console.log(values);
-
         if (values[0]) {
-            console.log("username found");
+            res.status(409).json({
+                success: false,
+                message: 'Error: a user with this username already exists'
+            });
+        } else if (values[1]) {
+            res.status(409).json({
+                success: false,
+                message: 'Error: a user with this email already exists'
+            });
         } else {
-            console.log("username available");
+            db.User.create(req.body).then(() => {
+                res.status(201).json({ message: 'You have successfully created a new account!' });
+            }).catch(err => {
+                console.log(err);
+            });
         };
-
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'Error occurred while creating a new account', error: err });
     });
 
-    // db.User.create(req.body).catch(err => {
-    //     console.log(err);
-    // });
 });
 
 module.exports = router;
