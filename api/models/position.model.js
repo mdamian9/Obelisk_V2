98fcf28bc372c203a861609ef1501327d143146a
Miguel Damian <38478936+mdamian9@users.noreply.github.com>
2023-01-10 21:24:23 -0800

    Create position model. Will come back to back - end. Will focus on front-end for a bit. Pending: create route for new position / test route w/ postman.

--- api/models/position.model.js
+++ api/models/position.model.js
@@ -1,32 +1,54 @@
 const mongoose = require('mongoose');
 
+// Come back to this, double check / create route.
 const positionSchema = mongoose.Schema({
     positionType: {
-
+        type: String,
+        required: true
     },
     asset: {
-
+        type: String,
+        required: true
+    },
+    assetTicker: {
+        type: String,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    },
+    currencyTicker: {
+        type: String,
+        required: true
     },
     quantity: {
-
+        type: Number,
+        required: true
     },
     entryPrice: {
-
+        type: Number,
+        required: true
     },
     exitPrice: {
-
+        type: Number,
+        required: true
     },
     leverage: {
-
+        type: Number,
+        required: true
     },
     status: {
-
+        type: Number,
+        required: true
     },
     pnl: {
-
+        type: Number,
+        required: true,
+        default: 0
     },
     date: {
-        type: String,
+        type: Date,
         required: true
     }
 });
